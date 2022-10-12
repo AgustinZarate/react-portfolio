7305592c86e6b689ea1fad2ccf325f4b60ee3cb5
Zarate Agustin <zarateagustin3@gmail.com>
2022-10-11 21:05:52 -0300

    Creacion de Sidebar

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './home.scss'
+import Sidebar from '../Sidebar/Sidebar';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Sidebar />
+    </div>
   )
 }
 
---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './sidebar.scss'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faBehance, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+
+function Sidebar() {
+  return (
+    <>
+    <div className="navbar">
+        <Link className='logo' to={'/'}>
+          <img src="./images/logo-a.png" alt="" />
+        </Link>
+        <nav>
+          <NavLink exact="true" activeclassname="true" className={"home-link"} to={"/"}>
+            <FontAwesomeIcon icon={faHome} color='#61DBFB'/>
+          </NavLink>
+          <NavLink exact="true" activeclassname="true" className={"about-link"} to={"/about"}>
+            <FontAwesomeIcon icon={faUser} color='#61DBFB'/>
+          </NavLink>
+          <NavLink exact="true" activeclassname="true" className={"contact-link"} to={"/contact"}>
+            <FontAwesomeIcon icon={faEnvelope} color='#61DBFB'/>
+          </NavLink>
+        </nav>
+
+    <ul>
+            <li>
+              <a target="_blank" className='' rel='noreferrer' href="#">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+            </li>
+            <li>
+              <a target="_blank" className='' rel='noreferrer' href="#">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </li>
+            <li>
+              <a target="_blank" className='' rel='noreferrer' href="#">
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
+            </li>
+            <li>
+              <a target="_blank" className='' rel='noreferrer' href="#">
+                <FontAwesomeIcon icon={faBehance} />
+              </a>
+            </li>
+          </ul>
+    </div>
+    </>
+  )
+}
+
+export default Sidebar
