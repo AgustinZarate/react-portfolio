4240ee5be92678456af049f2b21672582160630d
Zarate Agustin <zarateagustin3@gmail.com>
2022-10-12 22:03:35 -0300

    Comienzo de Home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './home.scss'
 
 
 function Home() {
   return (
-    <>
-      <div>
-        <h1>Hola Mundo</h1>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+              Hola, mi nombre es <br /> Agustin F. Zarate <br /> web developer
+          </h1>
+          <h2>React Frontend Developer jr</h2>
+          <Link to={"/contact"} className="flat-button">CONTACTAME</Link>
+        </div>
       </div>
-    </>
   )
 }
 
