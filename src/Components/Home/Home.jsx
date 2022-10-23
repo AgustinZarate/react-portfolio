60944e0cd48d08b783af598ea2cc5736877250a5
Zarate Agustin <zarateagustin3@gmail.com>
2022-10-23 18:02:07 -0300

    Animacion de letras en Home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './home.scss'
+import AnimateLetters from '../AnimatedLetters/AnimateLetters'
 
 
 function Home() {
+   /* Efecto maquina de escribir  */
   const [loopNum, setLoopNum ] = useState(0);
   const [isDeleting, setisDeleting ] = useState(false);
   const [text, setText] = useState('');
@@ -40,11 +42,31 @@ function Home() {
     }
   }
 
+/* Animacion de texto */
+  const [letterClass, setletterClass] = useState('text-animate')
+  const nameArray = ['A','g','u','s','t','i','n',' ','F.',' ','Z','a','r','a','t','e']
+
+useEffect(() => {
+  setTimeout(() => {
+    setletterClass('text-animate-hover')
+  }, 4000)
+}, [])
+
+
   return (
       <div className="container home-page">
         <div className="text-zone">
           <h1>
-              Hola, mi nombre es <br /> Agustin F. Zarate <br /> <span>{text}</span> 
+          <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <span> </span>
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+              {/* Hola, mi nombre es */} <br /> 
+              
+              <AnimateLetters letterClass={letterClass}  strArray={nameArray} idx={15} />
+
+              <br /> <span className='job'>{text}</span> 
           </h1>
           <Link to={"/contact"} className="flat-button">CONTACTAME</Link>
         {/* colocar h2 bienvenida */}
